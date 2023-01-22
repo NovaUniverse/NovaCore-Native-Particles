7046ca7a66a71994cb1d1d9e3fd363b3e8182dd8
Anton udén <udenanton@hotmail.com>
2023-01-22 19:55:16 +0100

    Update NativeNovaParticleProvider.java

--- src/net/novauniverse/novacorenativeparticles/implementation/NativeNovaParticleProvider.java
+++ src/net/novauniverse/novacorenativeparticles/implementation/NativeNovaParticleProvider.java
@@ -8,6 +8,7 @@ import org.bukkit.Particle;
 import org.bukkit.entity.Player;
 
 import net.zeeraa.novacore.spigot.abstraction.particle.NovaDustOptions;
+import net.zeeraa.novacore.spigot.abstraction.particle.NovaParticleEffect;
 import net.zeeraa.novacore.spigot.abstraction.particle.NovaParticleProvider;
 
 public class NativeNovaParticleProvider extends NovaParticleProvider {
@@ -31,5 +32,41 @@ public class NativeNovaParticleProvider extends NovaParticleProvider {
 	public void showRedstoneParticle(Location location, NovaDustOptions options, Predicate<Player> predicate) {
 		Particle.DustOptions data = new Particle.DustOptions(options.getColor(), options.getSize());
 		location.getWorld().getPlayers().stream().filter(predicate).forEach(receiver -> receiver.spawnParticle(Particle.REDSTONE, location.getX(), location.getY(), location.getZ(), 1, data));
+	}
+
+	private Particle mapParticle(NovaParticleEffect effect) {
+		switch (effect) {
+		case CRIT_MAGIC:
+			return Particle.CRIT_MAGIC;
+		case REDSTONE:
+			return Particle.REDSTONE;
+		case FIREWORKS_SPARK:
+			return Particle.FIREWORKS_SPARK;
+		case SMOKE_LARGE:
+			return Particle.SMOKE_LARGE;
+		case HEART:
+			return Particle.HEART;
+		}
+		return null;
+	}
+
+	@Override
+	public void showParticle(Location location, NovaParticleEffect effect) {
+		location.getWorld().spawnParticle(mapParticle(effect), location, 1);
+	}
+
+	@Override
+	public void showParticle(Location location, NovaParticleEffect effect, Player receiver) {
+		receiver.spawnParticle(mapParticle(effect), location, 1);
+	}
+
+	@Override
+	public void showParticle(Location location, NovaParticleEffect effect, Collection<Player> receivers) {
+		receivers.forEach(receiver -> receiver.spawnParticle(mapParticle(effect), location, 1));
+	}
+
+	@Override
+	public void showParticle(Location location, NovaParticleEffect effect, Predicate<Player> predicate) {
+		location.getWorld().getPlayers().stream().filter(predicate).forEach(receiver -> receiver.spawnParticle(mapParticle(effect), location, 1));
 	}
 }
