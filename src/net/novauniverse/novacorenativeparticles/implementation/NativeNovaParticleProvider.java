306cefe6f2eaae79a5cb06a109eba919bb253a4a
Anton udén <udenanton@hotmail.com>
2023-01-23 01:12:25 +0100

    Change particle function name

--- src/net/novauniverse/novacorenativeparticles/implementation/NativeNovaParticleProvider.java
+++ src/net/novauniverse/novacorenativeparticles/implementation/NativeNovaParticleProvider.java
@@ -13,23 +13,23 @@ import net.zeeraa.novacore.spigot.abstraction.particle.NovaParticleProvider;
 
 public class NativeNovaParticleProvider extends NovaParticleProvider {
 	@Override
-	public void showRedstoneParticle(Location location, NovaDustOptions options) {
+	public void showColoredRedstoneParticle(Location location, NovaDustOptions options) {
 		location.getWorld().spawnParticle(Particle.REDSTONE, location.getX(), location.getY(), location.getZ(), 1, new Particle.DustOptions(options.getColor(), options.getSize()));
 	}
 
 	@Override
-	public void showRedstoneParticle(Location location, NovaDustOptions options, Player receiver) {
+	public void showColoredRedstoneParticle(Location location, NovaDustOptions options, Player receiver) {
 		receiver.spawnParticle(Particle.REDSTONE, location.getX(), location.getY(), location.getZ(), 1, new Particle.DustOptions(options.getColor(), options.getSize()));
 	}
 
 	@Override
-	public void showRedstoneParticle(Location location, NovaDustOptions options, Collection<Player> receivers) {
+	public void showColoredRedstoneParticle(Location location, NovaDustOptions options, Collection<Player> receivers) {
 		Particle.DustOptions data = new Particle.DustOptions(options.getColor(), options.getSize());
 		receivers.forEach(receiver -> receiver.spawnParticle(Particle.REDSTONE, location.getX(), location.getY(), location.getZ(), 1, data));
 	}
 
 	@Override
-	public void showRedstoneParticle(Location location, NovaDustOptions options, Predicate<Player> predicate) {
+	public void showColoredRedstoneParticle(Location location, NovaDustOptions options, Predicate<Player> predicate) {
 		Particle.DustOptions data = new Particle.DustOptions(options.getColor(), options.getSize());
 		location.getWorld().getPlayers().stream().filter(predicate).forEach(receiver -> receiver.spawnParticle(Particle.REDSTONE, location.getX(), location.getY(), location.getZ(), 1, data));
 	}
